1f63b33f815fce70290775b16a1b51faed3ed636
yigitbaris <bariskaynakoglu01@gmail.com>
2024-11-02 21:14:45 +0300

    dashboard layout added

---
+++ client/src/app/(components)/Navbar/index.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
+      {/** search bar    */}
+      <div className="flex items-center gap-8"></div>
+    </div>
+  );
+};
+export default Navbar;
