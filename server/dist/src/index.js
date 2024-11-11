f8e5668366661bfce9e9774949a5671a6994d560
yigitbaris <bariskaynakoglu01@gmail.com>
2024-11-11 16:31:36 +0300

    project controller and route added

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -10,6 +10,7 @@ const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 // ROUTE IMPORTS
+const projectRoutes_1 = __importDefault(require("./routes/projectRoutes"));
 // CONFIGURATIONS
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -24,6 +25,7 @@ app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     res.send("This is home route");
 });
+app.use("/projects", projectRoutes_1.default);
 // SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
